83e098ef4758b3b97b197f708ce55fbd4c2bbddc
KendrickMcWha <kendrick.mcwha@outlook.com>
2021-04-02 19:13:09 -0400

    2021-04-02 b

---
+++ km-rest-api-server/km-rest-api-server/Controllers/RandomNumberController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace km_rest_api_server.Controllers
+{
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class RandomNumberController : ControllerBase
+    {
+        // GET: api/<RandomNumberController>
+        [HttpGet]
+        public string Get()
+        {
+            //return new string[] { "value1", "value2" };
+            Random rand = new Random();
+            int randNum = rand.Next( 100 );
+            return randNum.ToString();
+        }
+
+        // GET api/<RandomNumberController>/5
+        [HttpGet( "{id}" )]
+        public string Get( int id )
+        {
+            return "value";
+        }
+
+        // POST api/<RandomNumberController>
+        [HttpPost]
+        public void Post( [FromBody] string value )
+        {
+        }
+
+        // PUT api/<RandomNumberController>/5
+        [HttpPut( "{id}" )]
+        public void Put( int id, [FromBody] string value )
+        {
+        }
+
+        // DELETE api/<RandomNumberController>/5
+        [HttpDelete( "{id}" )]
+        public void Delete( int id )
+        {
+        }
+    }
+}
